711c340a231684d68784ac66bde99bd099dba5cb
Tommy Chheng <tommy@tumblr.com>
2015-09-01 09:26:08 -0700

    Initial Commit

---
+++ YelpAutolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YelpAutolayout
+//
+//  Created by Tommy Chheng on 9/1/15.
+//  Copyright (c) 2015 Tommy Chheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
